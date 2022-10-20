c2826696429952f105d40b1c042a92eceab12ad3
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2022-10-20 16:27:20 -0500

    services

---
+++ src/users/users.services.js
@@ -0,0 +1,41 @@
+const userControllers = require("../users/user.controllers");
+
+const getAllUsers = (req, res) => {
+  userControllers
+    .getAllUsers()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = req.params.id;
+  userControllers
+    .getUserById(id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(404).json({ message: "Invalid ID" });
+    });
+};
+
+const patchUser = (req, res) => {
+  const id = req.params.id;
+  const { firstName, lastName, phone, gender, country } = req.body;
+  userControllers
+    .updateUser(id, { firstName, lastName, phone, gender, country })
+    .then((data) => {
+      if (data[0]) {
+        res.status(200).json({ message: `User with ID ${id}, edited succesfully` });
+      } else {
+        res.status(404).json({ message: "Invalid ID" });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+};
---
+++ src/utils/crypto.js
@@ -0,0 +1,16 @@
+const bcrypt = require("bcrypt");
+
+//?Encrypt password
+const hashPassword = (plainPassword) => {
+  return bcrypt.hashSync(plainPassword, 10);
+};
+
+//? Compare passwords
+const comparePassword = (plainPassword, hashedPassword) => {
+  return bcrypt.compareSync(plainPassword, hashedPassword);
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};

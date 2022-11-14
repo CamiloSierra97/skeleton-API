5bc05b92cfdeb0f03ed0981c8ebd8a21cc978960
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2022-11-14 02:45:25 -0500

    env corrected

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const config = {
   nodeEnv: process.env.NODE_ENV || "development",
   jwtSecret: process.env.JWT_SECRET,
   db: {
-    host: process.env.BD_HOST || "localhost",
+    host: process.env.DB_HOST || "localhost",
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "root",
     dbName: process.env.DB_NAME,
